8fbc07d5d89e164a3b525363bb610fee13a089e0
Andrew Kochi <37052179+apkochi@users.noreply.github.com>
2018-04-01 16:49:48 -0400

    Add files via upload

---
+++ python Lessons and code.py
@@ -0,0 +1,46 @@
+#============ print "Hello World" =================
+print "Hello "
+
+
+#============ Lesson: Variables =================
+summation = 32 + 12
+subtraction = 55 - 10
+
+
+
+#============ Lesson: Variables =================
+# We've defined the variable "meal" here to be breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast")
+print(meal)
+
+# Now update meal to be lunch!
+meal = 
+
+# Printing out lunch
+print("Lunch")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner")
+print(meal)
+
+
+#============ Lesson: Updating Variables =================
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+october_rainfall = 7.20
+november_rainfall = 5.06
+december_rainfall = 4.06
